83a551bad3736889201d59a2eedf273bb4cec8ab
raydarnet <hansen.wu.85@gmail.com>
2017-02-05 18:15:50 -0800

    bash script to download course materials

---
+++ wgetcourse.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Download course materials from course site using recursive wget
+
+wget -v -l 10 -r -P ~/wget --no-check-certificate --reject="index.html?*" --header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11" https://www.cs.sjsu.edu/~austin/cs166-spring17/
