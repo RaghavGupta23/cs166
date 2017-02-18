c4821bec4f453d1b93b1b53fdbe0a186b2c586fa
raydarnet <hansen.wu.85@gmail.com>
2017-02-17 23:16:55 -0800

    add: tea encryption done, hw3 finished

--- hw/hw03/tea.c
+++ hw/hw03/tea.c
@@ -1,3 +1,9 @@
+/*
+ * Name:  Hansen Wu
+ * Class: CS 166-2
+ * Date:  2017-02-17
+ */
+
 #include <stdint.h>
 #include <stdio.h>
 
@@ -8,7 +14,28 @@
  * Also, note that the key is stored in 4 32-bit blocks.
  */
 void encrypt(uint32_t* text, uint32_t* key) {
-    /* YOUR CODE HERE */
+    // This is what we got
+    // printf("%02X\n", text[0]);
+    // printf("%02X\n", text[1]);
+    // printf("%02X\n", key[0]);
+    // printf("%02X\n", key[1]);
+    // printf("%02X\n", key[2]);
+    // printf("%02X\n", key[3]);
+
+    // Do some magic
+    // printf("%02LX\n", sum);
+    uint32_t delta = 0x9E3779B9;
+    // printf("%02LX\n", delta);
+    uint32_t sum = 0x0;
+
+    for (int i = 0; i < 32; ++i) {
+        sum += delta;
+        text[0] += ((text[1] << 4) + key[0]) ^ (text[1] + sum) ^ ((text[1] >> 5) + key[1]);
+        text[1] += ((text[0] << 4) + key[2]) ^ (text[0] + sum) ^ ((text[0] >> 5) + key[3]);
+    }
+
+    // return text;
+    // Encryption in place so nothing to return
 }
 
 /*
@@ -16,7 +43,16 @@ void encrypt(uint32_t* text, uint32_t* key) {
  * It should decrypt the text in place.
  */
 void decrypt(uint32_t* text, uint32_t* key) {
-    /* YOUR CODE HERE */
+    // Do some magic
+    uint32_t delta = 0x9E3779B9;
+    // printf("%02LX\n", delta);
+    uint32_t sum = delta << 5;
+
+    for (int i = 0; i < 32; ++i) {
+        text[1] -= ((text[0] << 4) + key[2]) ^ (text[0] + sum) ^ ((text[0] >> 5) + key[3]);
+        text[0] -= ((text[1] << 4) + key[0]) ^ (text[1] + sum) ^ ((text[1] >> 5) + key[1]);
+        sum -= delta;
+    }
 }
 
 /*
@@ -84,12 +120,36 @@ void cbc_mode_test() {
     print_arr(text, 10);
 
     /*************************** YOUR CODE HERE ***************************************/
-
+    // Ci = E(Pi ^ Ci-1, K)
+    text[0] ^= iv[0];
+    text[1] ^= iv[1];
+    encrypt(text, key);
+    for (int i = 2; i < 10; i += 2) {
+        text[i] ^= text[i-2];
+        text[i+1] ^= text[i-1];
+        encrypt(text + i, key);
+    }
 
     printf("(CBC) ciphertext= ");
     print_arr(text, 10);
 
     /*************************** YOUR CODE HERE ***************************************/
+    // Pi = D(Ci, K) ^ Ci-1
+    // Should be a way to save only current & prev ciphertext instead of all
+    // Previous attempt had persistent bugs though :(
+    uint32_t ciphertext[10];
+    for (int i = 0; i < 10; ++i)
+        ciphertext[i] = text[i];
+
+    decrypt(text, key);
+    text[0] ^= iv[0];
+    text[1] ^= iv[1];
+    for (int i = 2; i < 10; i += 2) {
+        decrypt(text + i, key);
+     
+        text[i] ^= ciphertext[i-2];
+        text[i+1] ^= ciphertext[i-1];
+    }
 
     printf("(CBC) plaintext = ");
     print_arr(text, 10);
@@ -121,12 +181,27 @@ void ctr_mode_test() {
     print_arr(text, 10);
 
     /*************************** YOUR CODE HERE ***************************************/
-
+    // Ci = Pi ^ E(IV - i, K)
+    for (int i = 0; i < 10; i += 2) {
+        iv[0] = iv_left - i;
+        iv[1] = iv_right - (i+1);
+        encrypt(iv,  key);
+        text[i] ^= iv[0];
+        text[i+1] ^= iv[1];
+    }
 
     printf("(CTR) ciphertext= ");
     print_arr(text, 10);
 
     /*************************** YOUR CODE HERE ***************************************/
+    // Pi = Ci ^ E(IV - i, K)
+    for (int i = 0; i < 10; i += 2) {
+        iv[0] = iv_left - i;
+        iv[1] = iv_right - (i+1);
+        encrypt(iv,  key);
+        text[i] ^= iv[0];
+        text[i+1] ^= iv[1];
+    }
 
     printf("(CTR) plaintext = ");
     print_arr(text, 10);
