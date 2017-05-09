4ffe65088c60dc57f18559059c907bc1decfcea6
raydarnet <hansen.wu.85@gmail.com>
2017-05-08 18:14:50 -0700

    add: problem 15 C code with unicode chars stripped

---
+++ hw11_12/15.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int diff, size = 8;
+    char *buf1, *buf2;
+    buf1 = (char *)malloc(size);
+    buf2 = (char *)malloc(size);
+    diff = buf2 - buf1;
+    memset(buf2, '2', size);
+    printf("BEFORE: buf2 = %s\n", buf2);
+    memset(buf1, '1', diff+3);
+    printf("AFTER: buf2 = %s\n", buf2);
+    return 0;
+}
